400e871aca2fc927d359b8f21a56ffe1dc42724a
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-06-17 23:34:23 +0530

    feat: restructure App component for authentication and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Signup from './pages/Signup/Signup.jsx'
+import Login from './pages/Login/Login.jsx'
+import Settings from './pages/Settings.jsx'
+import Profile from './pages/Profile/Profile.jsx'
+import { useAuthStore } from './store/useAuthStore.js'
+import { Loader } from "lucide-react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  console.log(authUser);
+
+  if (isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
+      </Routes>
+    </div>
   )
 }
 
