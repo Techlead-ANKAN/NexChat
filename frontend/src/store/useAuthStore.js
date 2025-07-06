c67acf0b89aa89c8c9f71e8379cf1a3bcd50a166
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-07-06 10:49:09 +0530

    working on the frontend logics

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authuser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authuser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.succes("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+}));
