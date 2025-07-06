dc4f275be588eafd2f9b0def7876a4535a2cf8e2
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-07-06 09:22:38 +0530

    ...

--- frontend/src/pages/Home/ModernHome.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ModernChatContainer from '../../components/modern/ModernChatContainer';
-
-function ModernHome() {
-  return <ModernChatContainer />;
-}
-
-export default ModernHome;
